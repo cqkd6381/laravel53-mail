a645e9b681aaf0e270733d5054764544a9388722
cqkd6381 <cqkd6381@163.com>
2017-05-06 01:14:30 +0800

    first commit

---
+++ resources/views/email/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome To Laravist</title>
+</head>
+<body>
+	<h1>Welcome To Laravist:{{$user->name}}</h1>
+	<p>Welcome To Laravist,my blog url is http://www.cqkd6381.com.</p>
+</body>
+</html>
